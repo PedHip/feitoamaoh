7fc32159bd6d1639c12f0567e66df86c64b31c47
pedhip <95312778+PedHip@users.noreply.github.com>
2024-11-27 17:21:23 -0300

    Update perfil.php

--- view/perfil.php
+++ view/perfil.php
@@ -21,7 +21,7 @@
     include '../components/logincadastro_modal.php';
     ?>
 	<main>
-		<div class="container">
+		<div class="secaoregistros">
 			<h2 class="text-center h1r mt-8">Carrinho de Compras</h2>
 	
 			<!-- Botões de Selecionar Todos e Desmarcar Todos -->
@@ -53,7 +53,7 @@
 			<!-- Contêiner para mensagens de sucesso ou erro -->
 			<div id="message-container" class="mt-3"></div>
 		</div>
-		<div class="container mt-5">
+		<div class="secaoregistros">
 			<h2 class="text-center h1r">Meus Pedidos</h2>
 			<div id="pedido-lista"></div>
 		</div>
