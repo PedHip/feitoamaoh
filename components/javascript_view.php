56d6b4444877b84aaa932355b481d2bfcaf7e4e8
pedhip <95312778+PedHip@users.noreply.github.com>
2024-11-27 13:44:49 -0300

    Update javascript_view.php

--- components/javascript_view.php
+++ components/javascript_view.php
@@ -162,6 +162,39 @@ if (isset($_SESSION['nome'])) {
                 });
             });
         });
+
+    $(document).ready(function() {
+                $('#formLogin').on('submit', function(event) {
+                    event.preventDefault();
+
+                    $.ajax({
+                        url: '../controllers/login.php',
+                        type: 'POST',
+                        data: $(this).serialize(),
+                        dataType: 'json', // Esperando uma resposta JSON
+                        success: function(response) {
+                            if (response.status === 'success') {
+                                $('#mensagem').html('<div style="color:green;">' + response.message + '</div>');
+                                // Redireciona após 1 segundo
+                                setTimeout(function() {
+                                    window.location.href = 'index.php'; // Mude para o caminho correto se necessário
+                                }, 1000);
+                            } else {
+                                $('#mensagem').html('<div style="color:red;">' + response.message + '</div>');
+                            }
+                        },
+                        error: function() {
+                            $('#mensagem').html('<div style="color:red;">Erro ao realizar login.</div>');
+                        }
+                    });
+                });
+
+
+                $('#lembrarSenha').on('click', function(event) {
+                    event.preventDefault();
+                    alert("Função de lembrar senha não implementada.");
+                });
+            });
 </script>
 
 
