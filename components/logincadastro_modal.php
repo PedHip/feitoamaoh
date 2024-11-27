8653ce015cc64e67eae42520656bf5c87cc3fcc3
pedhip <95312778+PedHip@users.noreply.github.com>
2024-11-27 14:03:32 -0300

    Update logincadastro_modal.php

--- components/logincadastro_modal.php
+++ components/logincadastro_modal.php
@@ -7,7 +7,7 @@
             <div class="cadastre">
                 <h1>Cadastre-se</h1>
                 <p>Não tem uma conta? Faça um cadastro para que possa realizar os seus pedidos</p>
-                <button type="submit" class="cadastreBttn" id="openModalLoginButton">criar conta</button>
+                <button type="submit" class="cadastreBttn" id="openModalCadastroButton">criar conta</button>
             </div>
             <form id="formLogin">
             <div class="data">
@@ -32,6 +32,33 @@
 
 <!-- Modal Cadastro -->
 <div class="modal fade" id="cadastroModal" tabindex="-1" aria-labelledby="cadastroModalLabel" aria-hidden="true">
- 
+    <main>
+		<section id="card2">
+			<div class="cadastre">
+				<h1>Bem-vindo de volta!</h1>
+				<p>Faça login no nosso site para voltar a fazer seus pedidos</p>
+				<button class="cadastreBttn" id="openModalLoginButton">sign in</button>
+			</div>
+			<div class="data">
+				<div class="login">
+					<h1>Cadastre-se</h1>
+					<p>Não tem uma conta? Faça um cadastro para que possa realizar os seus pedidos</p>
+				</div>
+				<form id="formCadastro">
+					<div id="dataDivInput">
+						<input class="input" type="text" id="nome" name="nome" placeholder="nome" required>
+						<input class="input" type="email" id="email" name="email" placeholder="email" required>
+						<input class="input" type="tel" id="telefone" name="telefone" placeholder="número" required maxlength="15" pattern="[0-9()+ \-]{1,15}" title="Apenas números e caracteres especiais (até 15 caracteres)">
+						<input class="input" type="password" id="senha" name="senha" placeholder="senha" required>
+					</div>
+					<span id="emailError" style="color:red; display:none;">Este email já está em uso.</span>
+					<div id="mensagem"></div>
+					<div id="dataDivBttn">
+						<button class="dataBttn" type="submit">criar conta</button>
+					</div>
+				</form>
+			</div>
+		</section>
+	</main>
 </div>
 
