b4b77417cc2c1329aa4128afbd2297d0378dda4b
pedhip <95312778+PedHip@users.noreply.github.com>
2024-11-27 14:00:55 -0300

    Update logincadastro_modal.php

--- components/logincadastro_modal.php
+++ components/logincadastro_modal.php
@@ -2,7 +2,32 @@
 
 <!-- Modal Login -->
 <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
-
+    <main>
+        <section id="card">
+            <div class="cadastre">
+                <h1>Cadastre-se</h1>
+                <p>Não tem uma conta? Faça um cadastro para que possa realizar os seus pedidos</p>
+                <button type="submit" class="cadastreBttn" id="openModalLoginButton">criar conta</button>
+            </div>
+            <form id="formLogin">
+            <div class="data">
+                <div id="display">
+                    <h1>Bem-vindo de volta!</h1>
+                    <p>Faça login no nosso site para voltar a fazer seus pedidos</p>
+                </div>
+                <div id="dataDivInput">
+                    <input class="input" type="email" id="email" name="email" placeholder="email" required>
+                    <input class="input" type="password" id="senha" name="senha" placeholder="senha" required>
+                </div>
+                <div id="dataDivBttn">
+                    <button type="submit" class="dataBttn">sign in</button>
+                </div>
+                <br>
+                <div id="mensagem"></div>
+            </div>
+            </form>
+        </section>
+     </main>
 </div>
 
 <!-- Modal Cadastro -->
