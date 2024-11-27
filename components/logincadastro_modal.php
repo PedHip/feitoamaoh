fd2ebcec9d0936530208c9285eea3e4a0b586152
pedhip <95312778+PedHip@users.noreply.github.com>
2024-11-27 14:09:49 -0300

    Update logincadastro_modal.php

--- components/logincadastro_modal.php
+++ components/logincadastro_modal.php
@@ -61,7 +61,7 @@
 </div>
 
 <!-- Modal Login -->
-<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+<div class="modal fade" id="loginModalMobile" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
     <main>
         <section id="card">
             <div class="cadastre">
@@ -91,7 +91,7 @@
 </div>
 
 <!-- Modal Cadastro -->
-<div class="modal fade" id="cadastroModal" tabindex="-1" aria-labelledby="cadastroModalLabel" aria-hidden="true">
+<div class="modal fade" id="cadastroModalMobile" tabindex="-1" aria-labelledby="cadastroModalLabel" aria-hidden="true">
     <main>
         <section id="card2">
             <div class="cadastre">
