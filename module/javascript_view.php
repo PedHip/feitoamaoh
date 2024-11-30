1f599b9858e5c6d4e7ee5f2dcc45f015611c795d
pedhip <95312778+PedHip@users.noreply.github.com>
2024-11-29 23:33:19 -0300

    Update javascript_view.php

--- module/javascript_view.php
+++ module/javascript_view.php
@@ -16,6 +16,7 @@ if (isset($_SESSION['nome'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRlNk1t2C8qC0ZPmvZGzTVL1BlxS4P4H8C6i9d6EJ" crossorigin="anonymous">
     <link rel="shortcut icon" type="imagex/png" href="../src/imagens/website/balloon.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../src/styles/styles.css">
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
