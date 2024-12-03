276204ddd04064dfbfd6d7023c42d684946e5037
pedhip <95312778+PedHip@users.noreply.github.com>
2024-12-03 00:03:55 -0300

    Update index.php

--- view/index.php
+++ view/index.php
@@ -247,11 +247,7 @@ if (isset($_SESSION['nome'])) {
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script>
-            var nomeUsuario = "<?php echo $nome_usuario; ?>";
 
-            document.getElementById("mensagemnome").innerHTML = nomeUsuario;
-    </script>
 
 </body>
 
