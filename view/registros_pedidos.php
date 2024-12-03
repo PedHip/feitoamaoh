4334efb9cd5649d0dd8bb76447372bafb08f731f
pedhip <95312778+PedHip@users.noreply.github.com>
2024-12-03 00:20:21 -0300

    Update registros_pedidos.php

--- view/registros_pedidos.php
+++ view/registros_pedidos.php
@@ -32,6 +32,7 @@ $usuarios = $usuario->listarUsuarios();
     <link rel="shortcut icon" type="imagex/png" href="../src/imagens/website/balloon.png">
     <link rel="stylesheet" href="../src/styles/backend.css"> 
      <link rel="stylesheet" href="../src/styles/styles.css">
+    <link rel="stylesheet" href="../src/styles/index.css"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js">
     <script src="../src/js/verificarusuario.js"></script>
