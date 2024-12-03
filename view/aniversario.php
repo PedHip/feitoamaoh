805071ec5cf2d3f5417c320df1485c3e6d888554
pedhip <95312778+PedHip@users.noreply.github.com>
2024-12-03 00:29:11 -0300

    Update aniversario.php

--- view/aniversario.php
+++ view/aniversario.php
@@ -20,7 +20,6 @@ if (isset($_SESSION['nome'])) {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="../src/styles/pedir.css">
     <link rel="stylesheet" href="../src/styles/produtos.css">
-    <link rel="stylesheet" href="../src/styles/backend.css">
     <link rel="stylesheet" href="../src/styles/index.css">
     <link rel="stylesheet" href="../src/styles/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
