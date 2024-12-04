89e018971fdd29e988ce24eb6c6451a7294ddfea
pedhip <95312778+PedHip@users.noreply.github.com>
2024-12-04 02:07:19 -0300

    Update footer.php

--- module/footer.php
+++ module/footer.php
@@ -3,12 +3,12 @@
             <div id="redeSocial">
                 <h3>Nossas redes sociais</h3>
                 <a href="https://www.instagram.com/feitoamaopontocom/">Instagram</a>
-                <a href="/">Whatsapp</a>
+                <a href="">Whatsapp</a>
             </div>
             <div id="contato">
                 <h3>Contato</h3>
-                <a href="/">(11)96914-1984</a>
-                <a href="/">feitoamao@gmail.com</a>
+                <a href="">(11)96914-1984</a>
+                <a href="">feitoamao@gmail.com</a>
             </div>
         </div>
         <div id="footerDireitos">
